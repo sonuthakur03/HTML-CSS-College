f79fa870fd40fb6252dc8080425618afdb0d8735
Sonu Thakur <136282638+sonuthakur03@users.noreply.github.com>
2025-04-18 06:44:58 +0545

    add changes

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,6 +2,11 @@
 const display = document.getElementById('result');
 const buttons = document.querySelectorAll('button');
 
+let opr = ['+','-','*','/'];
+let val1 = 0;
+let op = '';
+let str = '';
+
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         const value = button.textContent;
@@ -10,12 +15,19 @@ buttons.forEach(button => {
             display.value = '';
         } else if (value === '=') {
             try {
-                display.value = eval(display.value);
+                display.value = eval(str);
             } catch (error) {
                 display.value = 'Error';
             }
+        }else if(opr.includes(value)){
+            val1 = display.value;
+            display.value = '';
+            op = value;
+            str = val1 + op;
+            console.log(str);
         } else {
+            str += value; 
             display.value += value;
         }
     });
-});
+})
